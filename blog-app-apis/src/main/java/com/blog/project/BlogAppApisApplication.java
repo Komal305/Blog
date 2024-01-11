8cf6f9e3ebccbdeebac5a3a2ecc977bfb8e94654
Komal305 <komalrani2861@gmail.com>
2024-01-11 22:10:12 +0530

    add to index

--- blog-app-apis/src/main/java/com/blog/project/BlogAppApisApplication.java
+++ blog-app-apis/src/main/java/com/blog/project/BlogAppApisApplication.java
@@ -10,24 +10,25 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.blog.project.repositories", "com.blog.project.services.impl","com.blog.project.services","com.blog.project.config"})
+@ComponentScan(basePackages = { "com.blog.project.repositories", "com.blog.project.services.impl",
+		"com.blog.project.services", "com.blog.project.config" })
 public class BlogAppApisApplication implements CommandLineRunner {
-@Autowired
+	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApisApplication.class, args);
 	}
-	
+
 	@Bean
-public ModelMapper modelMapper() {
-	return new ModelMapper();
-}
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		System.out.println("encode password"+ passwordEncoder.encode("komal"));
+
+		System.out.println("encode password" + passwordEncoder.encode("komal"));
 	}
-	
+
 }
